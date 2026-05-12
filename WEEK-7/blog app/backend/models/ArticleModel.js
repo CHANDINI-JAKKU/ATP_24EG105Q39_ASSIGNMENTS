77df77ccba07518dce17f2479baa5394513a503f
CHANDINI-JAKKU <24eg105q39@anurag.edu.in>
2026-05-12 20:37:29 +0530

    Refactor ArticleModel and comment schema structure

--- WEEK-7/blog app/backend/models/ArticleModel.js
+++ WEEK-7/blog app/backend/models/ArticleModel.js
@@ -1,54 +1,47 @@
-import { Schema, model, Types } from "mongoose";
+import {Schema,model,Types} from 'mongoose'
 
-const commentSchema = new Schema({
-  user: {
-    type: Types.ObjectId,
-    ref: "user",
-    required: [true, "User ID required"],
-  },
-  comment: {
-    type: String,
-    required:[true,"Enter a comment"],
-  },
-});
+const commentSchema=new Schema({
+    user:{
+        type:Types.ObjectId,
+        ref:"user",
+        required:[true,"User ID is required"],   
+    },
+    comments:{
+        type:String,
+        required:[true,"Type Comment"]
+    }
+})
 
-const articleSchema = new Schema(
-  {
-    author: {
-      type: Types.ObjectId,
-      ref: "user",
-      required: [true, "Author ID is required"],
+
+const ArticleSchema= new Schema({
+    author:{
+        type:Types.ObjectId,
+        ref:"user",
+        required:[true,"Author ID is required"]
     },
-    title: {
-      type: String,
-      required: [true, "Title is required"],
+    title:{
+        type:String,
+        required:[true,"Title is required"]
     },
-    category: {
-      type: String,
-      required: [true, "Category is required"],
+    category:{
+        type:String,
+        required:[true,"Category is required"]
     },
-    content: {
-      type: String,
-      required: [true, "Content is required"],
+    content:{
+        type:String,
+        required:[true,"Content is required"]
     },
-    comments: [{ type: commentSchema, default: [] }],
+    comments:[{type: commentSchema, default:[]}],
     isArticleActive: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  {
-    versionKey: false,
-    timestamps: true,
-    strict: "throw",
-  },
-);
+        type: Boolean,
+        default:true
+    }
+},{
+    timestamps:true,
+    versionKey:false,
+    strict:"throw"
+})
 
-//create article model
-export const ArticleModel = model("article", articleSchema);
 
-// "mbsdf6sdf6df6sd6fs6dfs6df6sd"
-//ObjectId("bf7f7f7f7f7f7f77f7f")
-
-//{ comment:"",user:""}
-//find().populate("cart.product","pid productName brand")
+//create model
+export const ArticleModel = model("article",ArticleSchema)
