3b3149d2e07dcd57795aeeb38c265a54fcdacef9
CHANDINI-JAKKU <24eg105q39@anurag.edu.in>
2026-05-12 13:37:52 +0530

    Update Home component layout and styling

--- WEEK-7/blog app/frontend/src/components/Home.jsx
+++ WEEK-7/blog app/frontend/src/components/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-purple-700">
+        Welcome to Blog App
+      </h1>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
